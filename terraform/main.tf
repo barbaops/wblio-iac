58ae26dc9cbd981ae6addbd3172d461d4b00b359
Wallace Bruno Gentil <wallace@wbl.io>
2024-08-24 11:13:27 -0300

    add/tf_gtw_rts

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,25 +1,160 @@
-resource "aws_vpc" "main" {
-  cidr_block            = var.cidr_block
-  enable_dns_hostnames  = true
-  enable_dns_support    = true
-
-  tags = {
-    Name    = var.project
-    Project = var.project
-    Region  = var.region
-  }
+provider "aws" {
+  region = "us-east-1"
 }
 
-resource "aws_subnet" "subnets" {
-  for_each = var.subnets
+# VPC
+resource "aws_vpc" "main" {
+  cidr_block = var.vpc_cidr
+  tags = merge(var.tags, {
+    Name = "main-vpc"
+  })
+}
+
+# Internet Gateway
+resource "aws_internet_gateway" "main" {
+  vpc_id = aws_vpc.main.id
+  tags = merge(var.tags, {
+    Name = "main-igw"
+  })
+}
+
+# Public Subnets
+resource "aws_subnet" "public_subnets" {
+  for_each = { for idx, cidr in var.public_subnets : idx => cidr }
 
   vpc_id            = aws_vpc.main.id
-  cidr_block        = each.value.cidr_block
-  availability_zone = format("%s%s", var.region, each.value.availability_zone)
+  cidr_block        = each.value
+  availability_zone = element(var.availability_zones, each.key)
+  map_public_ip_on_launch = true
 
-  tags = {
-    Name    = format("%s-%s", var.region, each.key)
-    Project = var.project
-    Region  = var.region
+  tags = merge(var.tags, {
+    Name = "public-subnet-${each.key}"
+  })
+}
+
+# Private Subnets
+resource "aws_subnet" "private_subnets" {
+  for_each = { for idx, cidr in var.private_subnets : idx => cidr }
+
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = each.value
+  availability_zone = element(var.availability_zones, each.key)
+
+  tags = merge(var.tags, {
+    Name = "private-subnet-${each.key}"
+  })
+}
+
+# Databasse Subnets
+resource "aws_subnet" "databases_subnets" {
+  for_each = { for idx, cidr in var.database_subnets : idx => cidr }
+
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = each.value
+  availability_zone = element(var.availability_zones, each.key)
+
+  tags = merge(var.tags, {
+    Name = "databases-subnet-${each.key + 1}"
+  })
+}
+
+# Route Table for Public Subnets
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.main.id
   }
+
+  tags = merge(var.tags, {
+    Name = "public-route-table"
+  })
+}
+
+# Route Table Associations for Public Subnets
+resource "aws_route_table_association" "public_subnets" {
+  for_each = aws_subnet.public_subnets
+
+  subnet_id      = each.value.id
+  route_table_id = aws_route_table.public.id
+}
+
+# NAT Gateway
+resource "aws_eip" "nat" {
+  count = length(var.public_subnets)
+
+  vpc = true
+  tags = merge(var.tags, {
+    Name = "nat-eip-${count.index + 1}"
+  })
+}
+
+resource "aws_nat_gateway" "nat" {
+  count = length(var.public_subnets)
+
+  allocation_id = aws_eip.nat[count.index].id
+  subnet_id     = aws_subnet.public_subnets[count.index].id
+
+  tags = merge(var.tags, {
+    Name = "nat-gateway-${count.index + 1}"
+  })
+}
+
+# Route Table for Private Subnets
+resource "aws_route_table" "private" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.nat[0].id
+  }
+
+  tags = merge(var.tags, {
+    Name = "private-route-table"
+  })
+}
+
+# Route Table Associations for Private Subnets
+resource "aws_route_table_association" "private_subnets" {
+  for_each = aws_subnet.private_subnets
+
+  subnet_id      = each.value.id
+  route_table_id = aws_route_table.private.id
+}
+
+# Security Group - Allow All Traffic (for example purposes)
+resource "aws_security_group" "allow_all" {
+  vpc_id = aws_vpc.main.id
+
+  ingress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = merge(var.tags, {
+    Name = "allow-all-sg"
+  })
+}
+
+# Outputs
+output "vpc_id" {
+  value = aws_vpc.main.id
+}
+
+output "public_subnets" {
+  value = aws_subnet.public_subnets[*].id
+}
+
+output "private_subnets" {
+  value = aws_subnet.private_subnets[*].id
 }
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,33 +1,44 @@
-variable "cidr_block" {
+variable "vpc_cidr" {
   description = "CIDR block for the VPC"
   type        = string
+  default     = "10.0.0.0/16"
 }
 
-variable "project" {
-  description = "Project name for tagging"
-  type        = string
+variable "private_subnets" {
+  description = "List of CIDR blocks for private subnets"
+  type        = list(string)
+  default     = ["10.0.0.0/20", "10.0.16.0/20", "10.0.32.0/20"]
 }
 
-variable "region" {
-  description = "AWS region"
-  type        = string
+variable "public_subnets" {
+  description = "List of CIDR blocks for public subnets"
+  type        = list(string)
+  default     = ["10.0.48.0/24", "10.0.49.0/24", "10.0.50.0/24"]
 }
 
-variable "subnets" {
-  description = "Subnet configurations"
-  type = map(object({
-    cidr_block         = string
-    availability_zone  = string
-  }))
-  default = {
-    "private-1a"  = { cidr_block = "10.0.0.0/20",   availability_zone = "a" }
-    "private-1b"  = { cidr_block = "10.0.16.0/20",  availability_zone = "b" }
-    "private-1c"  = { cidr_block = "10.0.32.0/20",  availability_zone = "c" }
-    "public-1a"   = { cidr_block = "10.0.48.0/24",  availability_zone = "a" }
-    "public-1b"   = { cidr_block = "10.0.49.0/24",  availability_zone = "b" }
-    "public-1c"   = { cidr_block = "10.0.50.0/24",  availability_zone = "c" }
-    "database-1a" = { cidr_block = "10.0.51.0/24",  availability_zone = "a" }
-    "database-1b" = { cidr_block = "10.0.52.0/24",  availability_zone = "b" }
-    "database-1c" = { cidr_block = "10.0.53.0/24",  availability_zone = "c" }
+variable "database_subnets" {
+  description = "List of CIDR blocks for private subnets"
+  type        = list(string)
+  default     = ["10.0.51.0/20", "10.0.52.0/20", "10.0.53.0/20"]
+}
+
+variable "availability_zones" {
+  description = "List of availability zones"
+  type        = list(string)
+  default     = ["us-east-1a", "us-east-1b", "us-east-1c"]
+}
+
+variable "tags" {
+  description = "A map of tags to assign to resources"
+  type        = map(string)
+  default     = {
+    "Environment" = "dev"
+    "Team"        = "DevOps"
   }
+}
+
+variable "nat_gateway_enabled" {
+  description = "Enable or disable the NAT Gateway"
+  type        = bool
+  default     = true
 }
